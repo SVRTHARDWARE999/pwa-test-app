7fbe2164c12f42076b0bfc5334ab01d7bbc4499d
svrtplastics <156522814+SVRTHARDWARE999@users.noreply.github.com>
2024-02-29 22:17:03 +0530

    changes

--- java scripts/mrp.js
+++ java scripts/mrp.js
@@ -1,6 +1,7 @@
 function mrp() {
   
   // this is product 1 mrp
+  document.getElementById('dubc-01').innerHTML = "DRIZZLE Corner Shelf Super Clear | Unbreakable Transparent Multipurpose Use (Size 7 inch, 9 inch, 11 inch) - Set of 1 (3 Pieces)" ;
   document.getElementById('ubc-01').innerHTML = "₹280" ;
   document.getElementById('tubc-01').innerText = "₹280" ;
 }
