84ced7d552fefc8f03e2a155e75fc393e42cb037
svrtplastics <156522814+SVRTHARDWARE999@users.noreply.github.com>
2024-03-07 12:11:31 +0530

    svrt hardware

--- java scripts/mrp.js
+++ java scripts/mrp.js
@@ -3,5 +3,10 @@ function mrp() {
   // this is product 1 mrp
   document.getElementById('dubc-01').innerHTML = "DRIZZLE Corner Shelf Super Clear | Unbreakable Transparent Multipurpose Use (Size 7 inch, 9 inch, 11 inch) - Set of 1 (3 Pieces)" ;
   document.getElementById('ubc-01').innerHTML = "₹280" ;
-  document.getElementById('tubc-01').innerText = "₹280" ;
+  document.getElementById('tubc-01').innerHTML = "₹280" ;
+  
+  // this is product 2 mrp
+  document.getElementById('dakc-01').innerHTML = "Acrylic Wall Mount Bathroom Rack Blue (Pack of 3) (12x12 Inch) (30x30 cm)" ;
+  document.getElementById('akc-01').innerHTML = "₹1449" ;
+  document.getElementById('takc-01').innerHTML = "₹1449" ;
 }
